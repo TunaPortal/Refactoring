84dd0661133404b652fc8ca5a83b1bfc98e84b9f
권태현 <deletein@naver.com>
2023-08-03 23:10:16 +0900

    기본설정 완료

---
+++ tuna/src/main/java/co/refactoring/tuna/domain/main/dto/req/TestRequestVo.java
@@ -0,0 +1,11 @@
+package co.refactoring.tuna.domain.main.dto.req;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class TestRequestVo {
+}
